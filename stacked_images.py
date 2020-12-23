1838e006bfff4573292f3a0a11f8599284ab17c4
madhav <76476279+draco-git@users.noreply.github.com>
2020-12-23 21:45:57 +0530

    Update stacked_images.py

--- stacked_images.py
+++ stacked_images.py
@@ -1,44 +1,48 @@
 import cv2
 import numpy as np
-import import_images
 
-imgArray = import_images.allImages() # returns resource\image.png array
-#print(imgArray,len(imgArray))
-imgArray = np.array(imgArray)
+#reading various images
+#NOTE these images must ne stored in same directory where this file is stored
+img1 = cv2.imread('avengers.jpg')
+img2 = cv2.imread('car.jpg')
+img3 = cv2.imread('balls.jpg')
 
+#create a image_matrix which is 2D array which consists images 
+#The ouput will be shown in the order in image_matrix
+image_matrix = [[img1,img2,img3],[img1,img2,img3]]
+image_matrix1 = [[img1,img2,img3],[img1,img2]]
 
-def stacking(imgArray,matrix,scale,isGray):
-    if isGray == False:
-        imageMatrix = imgArray.reshape((matrix[1],matrix[0]))
-        imgArrays = [cv2.imread(i) for i in imgArray]
-        min_dim  = min([i.shape for i in imgArrays])
-        max_dim = max([i.shape for i in imgArrays])
-        avg_dim = (sum([(i.shape[0]) for i in imgArrays])//len(imgArrays),sum([(i.shape[1]) for i in imgArrays])//len(imgArrays))
-        print([i.shape for i in imgArrays])
-        print('min',min_dim,'max',max_dim,'avg',avg_dim)
-        same_size_imgArrays = [cv2.resize(i,(1080,720)) for i in imgArrays]
-        imgArrays = [cv2.resize(i,(0,0),None,scale,scale) for i in same_size_imgArrays]
-        n = matrix[1]
-        image_arrays=[]
-        for i in range(0,len(imgArrays),n):
-            image_arrays.append(imgArrays[i:i+n])
-        img = cv2.vconcat([cv2.hconcat(list_h) for list_h in image_arrays])
-        cv2.imshow('image_stacked',img)
-        cv2.waitKey(0)
-    else:
-        return('check the dimensions')
+#defining stacking function
+#stacking dunction consist of twon parameters 
+#image_matrix :- a matrix of images of same dimensions
+#scale :- a constant which decides the dimensions of images in output image i.e output_dim = input_dim * scale
+def stacking(image_matrix,scale): 
+    
+    same_dim_images_matrix = [] # A matrix which consists the images after modifying their dimensions such all image dimensions are same.
+    
+    max_len = max([len(i) for i in image)_matrix]) # max_len which stores the max len of row in image_matrix
+    for i in image_matrix:
+        if len(i) == max_len:
+            same_dim_images_matrix.append([cv2.resize(j,(512,512)) for j in i]) # resizing every image in image_matrix into same dim
+        else:
+            n = max_len-len(i) # if a row in image_matrix had less images
+            for count in range(n): # then fill the gap with the blank images
+                i.append(np.zeros((512,512,3)),np.uint8)
+            same_dim_images_matrix.append([cv2.resize(j,(512,512)) for j in i])
+            
+    scaled_image_matrix = [] # the matrix which stores the scaled images of same dimensions
+    for i in same_dim_images_matrix:
+        scaled_image_matrix.append([cv2.resize(j,None,scale,scale) for j in i])
+        
+    stacked_matrix = cv2.vconcat([cv2.hconcat(list_h) for list_h in scaled_image_matrix]) # the final result which contains stacked of images.
+    return stacked_matrix
 
+#calling the stacked function
+output = stacking(image_matrxi,0.5)
 
-#stacking(imgArray,[3,3],0.2)
-'''
-img = imgArray[0]
-
-image = cv2.imread(img)
-image  = cv2.resize(image,(256,256))
-gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-gray  = cv2.cvtColor(gray_image,cv2.COLOR_GRAY2BGR)
-im = cv2.hconcat([image,gray])
-cv2.imshow('stack',im)
+#display the image
+cv2.imshow('stacked_image',output)
 cv2.waitKey(0)
+cv2.displayAllWindows()
 
-'''
+        
